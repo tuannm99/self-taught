05750c5ce66a05fc604e74ed23457e5be5fffc8e
tuannm99 <gm.tuannm99@gmail.com>
2023-01-29 23:37:48 +0700

    finish boilplate

--- be/system/src/index.ts
+++ be/system/src/index.ts
@@ -1,31 +1,31 @@
 import bootstrap from './app.init';
 import { initialConsumers } from './consumer.init';
 import { ProducerSingleton } from './libs/kafka';
-import logger from './libs/winston';
+import { kafkaLogger, dbLogger } from './libs/winston';
 import { PostgresDataSource } from './models';
 
 const { producer } = ProducerSingleton.getInstance();
 
 producer.on('producer.connect', () => {
-  logger.info(`KafkaProvider: connected`);
+  kafkaLogger.info(`KafkaProvider: connected`);
 });
 
 producer.on('producer.disconnect', () => {
-  logger.info(`KafkaProvider: could not connect`);
+  kafkaLogger.info(`KafkaProvider: could not connect`);
 });
 
 producer.on('producer.network.request_timeout', (payload: any) => {
-  logger.info(`KafkaProvider: request timeout ${payload.clientId}`);
+  kafkaLogger.info(`KafkaProvider: request timeout ${payload.clientId}`);
 });
 
 (async () => {
   try {
     await PostgresDataSource.initialize();
-    logger.info('db connected');
+    dbLogger.info('db connected');
     await initialConsumers();
 
     await bootstrap();
   } catch (error) {
-    logger.error(error.toString());
+    dbLogger.error(error.toString());
   }
 })();
